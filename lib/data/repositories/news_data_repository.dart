8fea2b522abdcd943652222a9161f13a2fd5736e
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-09-23 11:22:08 +0100

    make the repo file clean

--- lib/data/repositories/news_data_repository.dart
+++ lib/data/repositories/news_data_repository.dart
@@ -12,34 +12,12 @@ class NewsDataRepository {
     try {
       final news = await newsWebServices.getResponse(modelUrl);
       final response = NewsData.fromJson(jsonDecode(news));
-      final articles = response.articles ?? [];
       
-      // Option 1: Get the last article as a Map
-      Map<String, dynamic>? jsonMap;
-      for (var article in articles) {
-        jsonMap = article.toJson(); // This is already a Map
-      }
-
-      print("Last article as Map: $jsonMap");
+      return response.articles ?? [];
       
-      // Option 2: Get all articles as a List of Maps
-      List<Map<String, dynamic>> allArticlesAsMap = [];
-      for (var article in articles) {
-        allArticlesAsMap.add(article.toJson());
-      }
-      
-      // Option 3: Using map() function (more concise)
-      List<Map<String, dynamic>> allArticlesMaps = articles
-          .map((article) => article.toJson())
-          .toList();
-
-      print("Total articles: ${articles.length}");
-      print("First article map: ${allArticlesMaps.isNotEmpty ? allArticlesMaps.first : 'No articles'}");
-      
-      return articles;
     } catch (e) {
       print('Error fetching articles: $e');
-      return []; // Return empty list on error
+      return [];
     }
   }
 }
