24cc01b55bcdb4cbf028626faaecd8b3947a551a
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-10-08 14:16:40 +0100

    fix 406 error of image

--- lib/presentation/widgets/news_card.dart
+++ lib/presentation/widgets/news_card.dart
@@ -14,30 +14,34 @@ class NewsCard extends StatelessWidget {
         Navigator.pushNamed(context, "/article", arguments: article);
       },
       child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         clipBehavior: Clip.hardEdge,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            article.urlToImage == null
-                ? Image.asset(
-                    "lib/assets/images/helper_images/no_image.jpg",
-                    height: 150,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  )
-                : Image.network(
-                    "${article.urlToImage}",
-                    height: 150,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
+            Image.network(
+              article.urlToImage ?? "",
+              height: 150,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Image.asset(
+                  "lib/assets/images/helper_images/no_image.jpg",
+                  height: 150,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                );
+              },
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                "${article.title}",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                article.title ?? "",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
               ),
@@ -45,8 +49,8 @@ class NewsCard extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8),
               child: Text(
-                "${article.source!.name}",
-                style: TextStyle(color: Colors.grey, fontSize: 12),
+                article.source?.name ?? "",
+                style: const TextStyle(color: Colors.grey, fontSize: 12),
               ),
             ),
           ],
