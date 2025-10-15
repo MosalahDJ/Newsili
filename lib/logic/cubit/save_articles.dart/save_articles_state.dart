2168f37d52871390228865c981fb450c23acd46a
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-10-15 16:07:43 +0100

    edit the saveartkle state

--- lib/logic/cubit/save_articles.dart/save_articles_state.dart
+++ lib/logic/cubit/save_articles.dart/save_articles_state.dart
@@ -1,10 +1,24 @@
 part of 'save_articles_cubit.dart';
 
 @immutable
-sealed class SaveArticlesState {}
-
-
+sealed class SaveArticlesState {
+  const SaveArticlesState();
+}
 
 final class SaveArticlesInitial extends SaveArticlesState {
-  final List<Articles> savedArticles = [];
+  const SaveArticlesInitial();
+}
+
+final class SaveArticlesloading extends SaveArticlesState {
+  const SaveArticlesloading();
+}
+
+final class SaveArticlesLoaded extends SaveArticlesState {
+  final List<Articles>? savedArticles;
+  const SaveArticlesLoaded({this.savedArticles});
+}
+
+final class SaveArticlesError extends SaveArticlesState {
+  final String? errorText;
+  const SaveArticlesError({this.errorText});
 }
