d52d57549ebbfeba3103d83d0e608a02019fe20e
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-12-03 15:38:51 +0100

    feat: add light and dark theme configurations with custom color accessors

---
+++ lib/helper/themes.dart
@@ -0,0 +1,24 @@
+// themes.dart
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+  scaffoldBackgroundColor: Colors.white,
+  useMaterial3: true,
+  appBarTheme: const AppBarTheme(centerTitle: true),
+);
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal, brightness: Brightness.dark),
+  scaffoldBackgroundColor: Colors.black,
+  useMaterial3: true,
+  appBarTheme: const AppBarTheme(centerTitle: true),
+);
+
+// Optional: custom colors accessor
+extension AppColors on ThemeData {
+  Color get cardBg => brightness == Brightness.dark ? Colors.grey[900]! : Colors.white;
+  Color get subtleText => brightness == Brightness.dark ? Colors.grey[400]! : Colors.grey[700]!;
+}
